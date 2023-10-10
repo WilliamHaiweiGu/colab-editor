b65a3a71484bcd35c93bf72876d49e51ec60e2e3
William Gu <williamhaiweigu@gmail.com>
2023-10-10 21:45:24 +0800

    implement subscribe_collab function

--- Client/client.js
+++ Client/client.js
@@ -2,26 +2,29 @@ import ShareDBClient from 'sharedb-client'
 import ReconnectingWebSocket from 'reconnecting-websocket';
 
 const sharedb_port = 8080;
-const rws = new ReconnectingWebSocket(`ws://localhost:${sharedb_port}`);
-const connection = new ShareDBClient.Connection(rws);
-const doc = connection.get('documents', 'plaintext');
+const subscribe_collab = html_edit_area => {
+    const rws = new ReconnectingWebSocket(`ws://localhost:${sharedb_port}`);
+    const connection = new ShareDBClient.Connection(rws);
+    const doc = connection.get('documents', 'plaintext');
 
+    doc.subscribe((err) => {
+        if (err) throw err;
+        if (doc.type === null)
+            doc.create({ content: '' });
 
-doc.subscribe((err) => {
-    if (err) throw err;
-    if (doc.type === null)
-        doc.create({ content: '' });
-
-    doc.on('op', () => {
-        console.log('Received change');
-        document.getElementById('editor').value = doc.data.content;
-        console.log('Applied received change');
+        doc.on('op', () => {
+            console.log('Received change');
+            html_edit_area.value = doc.data.content;
+            console.log('Applied received change');
+        });
     });
-});
 
-document.getElementById('editor').addEventListener('input', (e) => {
-    console.log('Detected change');
-    const op = [{ p: ['content'], oi: e.target.value }];
-    doc.submitOp(op);
-    console.log('Sent change');
-});
+    html_edit_area.addEventListener('input', (e) => {
+        console.log('Detected change');
+        const op = [{ p: ['content'], oi: e.target.value }];
+        doc.submitOp(op);
+        console.log('Sent change');
+    });
+}
+
+subscribe_collab(document.getElementById('editor'));
